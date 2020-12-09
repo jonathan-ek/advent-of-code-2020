0ff080381a920616d5b66afbdf8da7b793c4ff0b
jonathan <jonathan@jonathananderson.se>
2020-12-09 09:13:29 +0100

    Day 9

---
+++ day_09_1.py
@@ -0,0 +1,31 @@
+from helper import get_input, flatten
+
+
+def main():
+    data = [int(x) for x in get_input(9).split('\n') if x]
+    preamble_length = 25
+    preamble = data[:preamble_length]
+    sums = []
+    for first_nr in preamble:
+        day_sum = []
+        for second_nr in preamble:
+            if first_nr != second_nr:
+                day_sum.append(first_nr + second_nr)
+        sums.append(day_sum)
+    result = None
+    for index, nr in enumerate(data):
+        if index < preamble_length:
+            continue
+        if nr not in flatten(sums):
+            print(nr)
+            break
+        sums = sums[1:]
+        new_day = []
+        for second_nr in data[index - preamble_length: index]:
+            if nr != second_nr:
+                new_day.append(nr + second_nr)
+        sums.append(new_day)
+
+
+if __name__ == '__main__':
+    main()
--- helper.py
+++ helper.py
@@ -2,3 +2,7 @@ def get_input(day, post_fix=1):
     """Read the input file and return the content as a string."""
     with open(f'input/day_{day:02d}_{post_fix}.txt', 'r') as data:
         return data.read()
+
+
+def flatten(data):
+    return [item for sublist in data for item in sublist]
